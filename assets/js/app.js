ef6b81c25c61de05b17c503b984cb104e07beff7
Rivo Hermann <rahajasonr@proximity.bbdo.fr>
2018-10-01 15:59:46 +0400

    integrate welcome page

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,10 +7,11 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 import '../css/app.scss';
+import '../css/global.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 import $ from 'jquery';
-import greet from './greet';
+// import greet from './greet';
 
 // JS is equivalent to the normal "bootstrap" package
 // no need to set this to a variable, just require it
